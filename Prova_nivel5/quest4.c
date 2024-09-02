337f3b5864a123097f8fe29bb1d9ad6ee9e45e48
Kresley Lucas <96209978+klucas27@users.noreply.github.com>
2024-09-01 23:15:22 -0300

    lucas

--- Prova_nivel5/quest4.c
+++ Prova_nivel5/quest4.c
@@ -1,1 +1,24 @@
-/*Difícil: Escreva um programa que leia um número inteiro positivo e exiba todos os seus divisores.*/
+/*Difícil: Escreva um programa que leia um 
+número inteiro positivo e exiba todos os seus divisores.*/
+
+
+#include <stdio.h>
+
+int main(){
+
+    int num;
+    int retira = 1;
+
+    printf(">> ");
+    scanf("%d", &num);
+
+    while (retira <= num){
+        if (num % retira == 0){
+            printf("\n%d",retira);
+        };
+
+        retira ++;
+    }
+
+    return 0;
+}
--- Prova_nivel5/quest5.c
+++ Prova_nivel5/quest5.c
@@ -1,1 +1,3 @@
-/*Difícil: Crie um programa que leia uma sequência de números inteiros e exiba a soma dos números positivos, a soma dos números negativos, e a soma total de todos os números lidos. A leitura deve parar quando o número 0 for digitado.*/
+/*Difícil: Crie um programa que leia uma sequência de números inteiros e 
+exiba a soma dos números positivos, a soma dos números negativos, 
+e a soma total de todos os números lidos. A leitura deve parar quando o número 0 for digitado.*/
