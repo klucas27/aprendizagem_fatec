ff6ef963b344874d76d0f9440aff4ce673a1d37a
Kresley Lucas <lucas27ro@gmail.com>
2024-09-15 22:25:28 -0300

    Atividade_3

---
+++ Fatec/Algotitmo/Atividade_3/KRESLEY LUCAS ROMAO RIBEIRO 1° GTI FATEC/exercicio6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+
+    int i = 1;
+    while(i<=10){
+        printf("%d   %d   %d\n", i, i, i);
+        i++;
+    }
+
+    system("Pause");
+    return 0;
+}
